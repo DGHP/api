b0deb786e49a8432a314c13ab45fb1e448c9c2a1
Ivo Evans <ievans.storrie@gmail.com>
2020-04-30 11:34:30 +0100

    Add iat and exp to JWTs

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ import json
 import jwt
 from flask import request
 from werkzeug.security import generate_password_hash, check_password_hash
+from time import time
 
 from app import app
 # create user
@@ -28,6 +29,7 @@ users = [
 
 @app.route('/get-users')
 def get_users():
+    print(time())
     return json.dumps(users)
 
 
@@ -36,23 +38,23 @@ def add_user():
     user_dictionary = request.get_json()
     user_dictionary['password'] = generate_password_hash(user_dictionary['password'])
     users.append(request.get_json())
-    return json.dumps(users) # return JWT
+    return json.dumps(users) 
 
 @app.route('/login', methods=["PUT"])
 def login():
     body = request.get_json()
     username = body["name"]
     password = body["password"]
+    now = int(time())
+    week_later = now + 604800
     for u in users:
         if u['name'] == username and check_password_hash(u['password'], password):
-            return jwt.encode({'username': username}, jwt_secret, algorithm="HS256")
-    return "Your credentials have been rejected" # return JWT
+            return jwt.encode({'username': username, 'iat': now, 'exp': week_later}, jwt_secret, algorithm="HS256")
+    return "Your credentials have been rejected" 
 
 @app.route('/make-change', methods=['POST'])
 def do_something():
     body = request.get_data()
     credentials = jwt.decode(body, jwt_secret, algorithms=['HS256'])
     return credentials
-# @app.route('verify-jwt')
-# def verify():
 
