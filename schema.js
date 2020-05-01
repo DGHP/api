05fe3bd121649a3c1cc23fefd026e9fd5700517c
Ivo Evans <ievans.storrie@gmail.com>
2020-05-01 14:34:31 +0100

    Create schema.js file to discuss database structure

---
+++ schema.js
@@ -0,0 +1,35 @@
+// this is basically for JSON. It's a .js file so we can have comments
+// not an actual schema just for discussion of what needs to go into game objects
+
+const game = {
+	name: "fac19",
+	playerCount: "8",
+    mode: "original",
+    characterDeck: [],
+    districtDeck: [],
+    turn: 0, // we can sort playerUsernames by the value of their characterRole, then use turn to look up the specific player we're interested in this turn
+    stage: "card-selection",
+	playerUsernames: [
+		{monkey: {
+			districtsInHand: ["fortress", "castle", "museum", "graveyeard"],
+			goldCount: 2,
+			districtsBuilt: [],
+			characterRole: "",
+			totalPoints: 0
+		}},
+		{ivo: {
+			districtsInHand: ["jail", "castle", "church", "statue"],
+			goldCount: 2,
+			districtsBuilt: [],
+			characterRole: "",
+			totalPoints: 0
+		}},
+		{ako: {
+			districtsInHand: ["trade mill", "castle", "museum", "statue"],
+			goldCount: 2,
+			districtsBuilt: [],
+			characterRole: "",
+			totalPoints: 0
+		}}
+	]
+}
