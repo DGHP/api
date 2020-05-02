68598d6a95497e303887da87d16de536361c9543
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-05-02 22:49:36 +0100

    heroku deploy

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
 from pymongo import MongoClient
 
-client = MongoClient()
+# on heroku there is variable setted up with MONGO_URI key and db url as a value
+client = MongoClient(os.environ.get('MONGO_URI'))
 app = Flask(__name__)
 CORS(app)
 db = client.dev_database
