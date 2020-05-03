b3b32e851317114f1e0a005aab8de754be2d2c15
Ivo Evans <ievans.storrie@gmail.com>
2020-05-03 23:20:01 +0100

    Add shell context variables

--- api.py
+++ api.py
@@ -1,1 +1,6 @@
 from app import app
+from app.models import games, users, get_from_database
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'games': games, 'users': users, 'get_from_database': get_from_database}
